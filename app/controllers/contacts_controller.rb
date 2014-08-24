6903e51699a8d9fe2fa3aee90d6949df1de3834d
Ahmed O <aomra015@gmail.com>
2014-08-24 15:07:31 -0400

    add controller code - API now responds with JSON

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,58 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :update, :destroy]
+
+  # GET /contacts
+  # GET /contacts.json
+  def index
+    @contacts = Contact.all
+
+    render json: @contacts
+  end
+
+  # GET /contacts/1
+  # GET /contacts/1.json
+  def show
+    render json: @contact
+  end
+
+  # POST /contacts
+  # POST /contacts.json
+  def create
+    @contact = Contact.new(params[:contact])
+
+    if @contact.save
+      render json: @contact, status: :created, location: @contact
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /contacts/1
+  # PATCH/PUT /contacts/1.json
+  def update
+    if @contact.update(params[:contact])
+      head :no_content
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /contacts/1
+  # DELETE /contacts/1.json
+  def destroy
+    @contact.destroy
+
+    head :no_content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def contact_params
+      params.require(:contact).permit(:name, :email, :twitter)
+    end
 end
