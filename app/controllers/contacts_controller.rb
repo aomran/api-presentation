3d57f00aab42655b6f4a611f88283034b33a5e6a
Ahmed O <aomra015@gmail.com>
2014-08-24 15:14:15 -0400

    fix strong params issue

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -22,7 +22,7 @@ class ContactsController < ApplicationController
   # POST /contacts
   # POST /contacts.json
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
 
     if @contact.save
       render json: @contact, status: :created, location: @contact
@@ -34,7 +34,7 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   # PATCH/PUT /contacts/1.json
   def update
-    if @contact.update(params[:contact])
+    if @contact.update(contact_params)
       head :no_content
     else
       render json: @contact.errors, status: :unprocessable_entity
