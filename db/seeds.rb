01dc439978b08e9ecb6e5e0432d61b84776565c6
Ahmed O <aomra015@gmail.com>
2014-08-25 17:45:06 -0400

    add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Contact.create(
+  [ { name: "Jeremy Kemper", twitter: "@bitsweat", relationship: "friend" },
+    { name: "David Heinemeir Hansson", twitter: "@dhh", relationship: "business" },
+    { name: "Jose Valim", twitter: "@josevalim", relationship: "business" },
+    { name: "Aaron Patterson", twitter: "@tenderlove", relationship: "business" },
+    { name: "Josh Peek", twitter: "@joshp", relationship: "business" },
+    { name: "Xavier Noria", twitter: "@fxn", relationship: "business" },
+    { name: "Yehuda Katz", twitter: "@wycats", relationship: "business" },
+    { name: "Patrik Naik", twitter: "@lifo", relationship: "friend" },
+    { name: "Santiago Pastorino", twitter: "@spastorino", relationship: "business" },
+    { name: "Carl Lerche", twitter: "@carllerche", relationship: "business" },
+    { name: "Emilio Tagua", twitter: "@miloops", relationship: "business" },
+    { name: "Ryan Bigg", twitter: "@ryanbigg", relationship: "business" }
+  ])
